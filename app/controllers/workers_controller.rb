3f7f4c7a65f254e902d9db27e401e29de143ebd7
mori takuto <mr04vv@gmail.com>
2018-04-08 01:51:08 +0900

    / /client実装

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,6 +1,5 @@
 class WorkersController < ApplicationController
   def index
-    user_id = current_user.id
     @recruits = Recruit.all
     user_ids = ClientRecruitRelation.getUserIdByRecruitId(@recruits)
     @rank = Rank.clientRankAverage(user_ids)
