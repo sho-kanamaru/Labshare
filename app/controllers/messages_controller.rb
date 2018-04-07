3d4eba7ae61293aa92df326fdb262468268e7098
WallyNegi <u.str.gm@gmail.com>
2018-04-08 07:52:25 +0900

    メッセージ画面のロジック修正.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,10 +11,15 @@ class MessagesController < ApplicationController
   def show
     @message = Message.new
     @recruit = Recruit.find(params[:recruit_id])
-    @user = get_recruit_maker_user
-    @messages = Message.where(from_id: current_user.id, to_id: @user.id).order("created_at ASC")
-    @users = WorkerRecruitRelation.where(recruit_id: @recruit).map{ |user| User.find(user.user_id) }
-    if WorkerRecruitRelation.where(user_id: current_user.id, recruit_id: params[:recruit_id]).size == 0
+
+    # 求人案件を作製した人
+    @client = get_recruit_maker_user
+    @messages = Message.where(recruit_id: @recruit.id).order("created_at ASC")
+
+    # 応募者
+    @users = get_applied_users
+
+    if WorkerRecruitRelation.where(user_id: current_user.id, recruit_id: params[:recruit_id]).size == 0 && @client != current_user
       WorkerRecruitRelation.create(user_id: current_user.id, recruit_id: params[:recruit_id])
     end
   end
@@ -23,7 +28,13 @@ class MessagesController < ApplicationController
 
   def get_recruit_maker_user
     recruit_maker = ClientRecruitRelation.where(recruit_id: params[:recruit_id])
-    recruit_maker_user = User.find(recruit_maker[0].id)
+    recruit_maker_user = User.find(recruit_maker[0].user_id)
+  end
+
+  private
+  def get_applied_users
+    applied_users = WorkerRecruitRelation.where(recruit_id: params[:recruit_id])
+    users = applied_users.map{ |user| User.find(user.user_id)}
   end
 
   def create_params
