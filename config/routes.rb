fb03f05345d2b53103c53d0727d93f20a98111f6
sho <sho02.soccer@gmail.com>
2018-04-07 21:46:53 +0900

    create recruit func

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   get 'workers/index'
 
   root "workers#index"
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # clients
   resources :clients, only: [:index]
 
-  resources :recruits, only: [:new, :show] do
+  resources :recruits, only: [:new, :create, :show] do
     resources :messages, only: [:index]
   end
 end
