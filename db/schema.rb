0ce85052391bbde22628811a676c1147030b70d9
sho <sho02.soccer@gmail.com>
2018-04-08 10:51:59 +0900

    add div

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180408003721) do
+ActiveRecord::Schema.define(version: 20180408010807) do
 
   create_table "client_recruit_relations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20180408003721) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "recruit_money", default: 0
+    t.text "image"
     t.index ["field_id"], name: "index_recruits_on_field_id"
   end
 
